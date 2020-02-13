6e4f742aac8bd3b4eb8474bed417098156d9effd
swansonkrs <60860197+swansonkrs@users.noreply.github.com>
2020-02-13 09:15:27 -0700

    Revert "AE Challenge"
    This reverts commit 953aa4c3631c17665e05cf26e904693f872f7655.

--- AEConsoleApp/ae_code_challenge/dbo/Views/vw_avg_per_hour.sql
+++
@@ -1,6 +0,0 @@
-﻿
-
-CREATE VIEW [dbo].[vw_avg_per_hour] AS
-SELECT ErrorCode, COUNT(*) / CASE WHEN DATEDIFF(hour, MIN(StartTime), MAX(StartTime)) < 1 THEN 1 ELSE DATEDIFF(hour, MIN(StartTime), MAX(StartTime)) END as avgperhour
-FROM server_response_log
-GROUP  BY ErrorCode
